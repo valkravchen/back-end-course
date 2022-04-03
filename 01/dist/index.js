5d75fb9b6fee075dece72210d4d3e5dccc530ca8
valkravchen <100293539+valkravchen@users.noreply.github.com>
2022-04-03 22:15:25 +0300

    add new files

--- 01/dist/index.js
+++ 01/dist/index.js
@@ -1,9 +1,13 @@
 "use strict";
-const express = require('express');
-const app = express();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
 const port = 5000;
 app.get('/', (req, res) => {
-    let helloMessage = 'Hello Incubator';
+    let helloMessage = 'Hello Incubator!';
     res.send(helloMessage);
 });
 app.listen(port, () => {
--- 01/src/index.ts
+++ 01/src/index.ts
@@ -1,9 +1,9 @@
-const express = require('express')
+import express, {Request, Response} from  'express'
 const app = express()
 const port = 5000
 
-app.get('/', (req: any, res: any) => {
-    let helloMessage = 'Hello Incubator'
+app.get('/', (req: Request, res: Response) => {
+    let helloMessage = 'Hello Incubator!'
     res.send(helloMessage)
 })
 

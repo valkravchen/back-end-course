509e66ea98d071fff43e91c9af3008be71d4cc21
valkravchen <100293539+valkravchen@users.noreply.github.com>
2022-04-09 19:17:56 +0300

    changes

---
+++ 01/src/index.ts
@@ -0,0 +1,12 @@
+import express, {Request, Response} from 'express'
+const app = express()
+const port = 3000
+
+app.get('/', (req: Request, res:Response) => {
+    let helloMessage = 'Первый урок закончен.'
+    res.send(helloMessage)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
